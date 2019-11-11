3a7f2a02d1c4b1baff4abdc13917293953932867
Aluno <sidney.t.oku@gmail.com>
2019-11-11 20:24:09 -0300

    Refactoring 5: Extract Method

--- AulaPraticaRefectoring/src/br/com/sidney/entidades/Customer.java
+++ AulaPraticaRefectoring/src/br/com/sidney/entidades/Customer.java
@@ -51,25 +51,8 @@ public class Customer {
 		return result;
 	}
 	
-	public double amountFor(Rental aRental) {
-		double thisAmount= 0;
-		// determine amounts for each line
-		switch (aRental.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (aRental.getDaysRented() > 2)
-				thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			thisAmount += aRental.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (aRental.getDaysRented() > 3)
-				thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-			break;
-		}
-		
-		return thisAmount;
+	private double amountFor(Rental aRental) {
+	      return aRental.getCharge();  // agora apenas delega chamada para método movido
 	}
+	
 }
