1348a8f77afd0f5483acf9078e77f7531f394fbb
Aluno <sidney.t.oku@gmail.com>
2019-11-11 20:56:36 -0300

    Nova feature: Statement em HTML

--- AulaPraticaRefectoring/src/br/com/sidney/entidades/Customer.java
+++ AulaPraticaRefectoring/src/br/com/sidney/entidades/Customer.java
@@ -38,6 +38,27 @@ public class Customer {
 		
 		return result;
 	}
+	
+	public String htmlStatement() {
+		Enumeration rentals = rentalVector.elements();
+		String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+
+			// show figures for this rental
+			result += each.getMovie().getTitle()+ ": " +
+	                String.valueOf(each.getCharge()) + "<BR>\n";
+		}
+
+		// add footer lines
+		result +=  "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+		result += "On this rental you earned " 
+					+ "<EM>" + String.valueOf(getTotalFrequentRenterPoints()) + "</EM>"
+					+ " frequent renter points<P>";
+		
+		return result;
+	}
 
 	private double getTotalCharge() {
 		double result = 0;
